dc53fc7f712ed04eea5f3693d3d67e8ef7daa6d2
Sanjots947@outlook.com <Sanjots947@outlook.com>
2024-06-17 10:44:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using RestSharp;
+using System.Threading;
+using System.Threading.Tasks;
+namespace HelloWorldApplication
+{
+	class HelloWorld
+	{
+		static async Task Main(string[] args)
+		{
+			var options = new RestClientOptions("https://reqres.in");			
+			var client = new RestClient(options);
+			var request = new RestRequest("/api/users?page=1", Method.Get);
+			var body = @"";
+			request.AddParameter("text/plain", body, ParameterType.RequestBody);
+			RestResponse response = await client.ExecuteAsync(request);
+			Console.WriteLine(response.Content);
+		}
+		static async Task Main1(string[] args)
+		{
+
+			var options = new RestClientOptions("https://reqres.in");
+			var client = new RestClient(options);
+			var request = new RestRequest("/api/users/5", Method.Get);
+			RestResponse response = await client.ExecuteAsync(request);
+			Console.WriteLine(response.Content);
+		}
+		static async Task Main2(string[] args)
+		{
+			var options = new RestClientOptions("https://reqres.in");
+			var client = new RestClient(options);
+			var request = new RestRequest("/api/users?page=1",Method.Get);
+			RestResponse response = await client.ExecuteAsync(request);
+			Console.WriteLine(response.Content);
+		}
+		static async Task Main3(string[] args)
+		{
+
+			var options = new RestClientOptions("https://reqres.in");
+			var client = new RestClient(options);
+			var request = new RestRequest("/api/users", Method.Post);
+			request.AddHeader("Content-Type", "text/html");
+			var body = @"{
+            " + "\n" +
+			@"   ""id"":13
+            " + "\n" +
+			@"   ""email"" : ""brandonwoods@gmail.com"",
+            " + "\n" +
+			@"   ""first_name"" : "" Brandon "",
+            " + "\n" +
+			@"   ""last_name"" : "" Wood ""
+            " + "\n" +
+			@"}";
+			request.AddParameter("text/html", body, ParameterType.RequestBody);
+			RestResponse response = await client.ExecuteAsync(request);
+			Console.WriteLine(response.Content);
+		}
+		static async Task Main4(string[] args)
+		{
+
+			var options = new RestClientOptions("https://reqres.in");
+			var client = new RestClient(options);
+			var request = new RestRequest("/api/users/315", Method.Put);
+			request.AddHeader("Content-Type", "text/html");
+			var body = @"{
+            " + "\n" +
+			@"   ""id"":13
+            " + "\n" +
+			@"   ""email"" : "" brandonwoods@gmail.com"",
+            " + "\n" +
+			@"   ""first_name"" : "" Bradnon "",
+            " + "\n" +
+			@"   ""last_name"" : ""Woods Biden ""
+            " + "\n" +
+			@"}";
+			request.AddParameter("text/html", body, ParameterType.RequestBody);
+			RestResponse response = await client.ExecuteAsync(request);
+			Console.WriteLine(response.Content);
+		}
+	}
+}
